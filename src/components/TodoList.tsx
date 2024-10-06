d0d82acbd0115d731dc51bcd62254d094825ad3b
hosam reda <rdhosam712@gmail.com>
2024-10-06 03:06:30 +0300

    update todo modal

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,16 @@
 import Button from "./ui/Button";
 import { ITodo } from "../interfaces";
 import useAuthenticatedQuery from "../hooks/useAuthenticatedQuery";
+import Modal from "./ui/Modal";
+import { useState } from "react";
+import Input from "./ui/Input";
 
 export const TodoList = () => {
   const storageKey = "loggedInUser";
   const userDataString = localStorage.getItem(storageKey);
   const userData = userDataString ? JSON.parse(userDataString) : null;
 
+  const [isEditModal, setIsEditModal] = useState(false);
   const { isLoading, data } = useAuthenticatedQuery({
     queryKey: ["todos"],
     url: "/users/me?populate=todos",
@@ -19,6 +23,11 @@ export const TodoList = () => {
 
   console.log({ data });
 
+  // ** Handlers
+  const onTaggleEditModal = () => {
+    setIsEditModal((prev) => !prev);
+  };
+
   if (isLoading) return <h3>Loading...</h3>;
 
   return (
@@ -29,9 +38,13 @@ export const TodoList = () => {
             key={todo.id}
             className="flex itmes-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md"
           >
-            <p className="w-full font-semibold">1 - {todo.title}</p>
+            <p className="w-full font-semibold">
+              {todo.id} - {todo.title}
+            </p>
             <div className="flex items-center justify-end w-full space-x-3">
-              <Button size={"sm"}>Edit</Button>
+              <Button size={"sm"} onClick={onTaggleEditModal}>
+                Edit
+              </Button>
               <Button variant={"danger"} size={"sm"}>
                 Remove
               </Button>
@@ -41,6 +54,19 @@ export const TodoList = () => {
       ) : (
         <h3>No Todos </h3>
       )}
+      <Modal
+        isOpen={isEditModal}
+        closeModal={onTaggleEditModal}
+        title="Edit this todo"
+      >
+        <Input value="EDIT TODO" />
+        <div className="flex items-center space-x-3 mt-4">
+          <Button className="bg-indigo-700 hover:bg-indigo-800">Update</Button>
+          <Button variant={"cancel"} onClick={onTaggleEditModal}>
+            Cancel
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 };
